ec2ad4b633bef4b58e4acc10ed2fd00d4f31d8c9
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-05-07 07:37:40 +0800

    add DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace e_ticket.Data
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_ticket.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ActorId { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
