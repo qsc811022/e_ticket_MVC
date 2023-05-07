b6f4ac033c7940cfe771a49ddffe95991399cfef
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-05-07 08:54:20 +0800

    add startup cs

---
+++ Startup.cs
@@ -0,0 +1,57 @@
+﻿using e_ticket.Data;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace e_ticket
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // 注册服务
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // 在此处注册其他服务
+            // 例如：services.AddScoped<IMyService, MyService>();
+            services.AddDbContext<AppDbContext>();
+        }
+
+        // 配置 HTTP 请求管道
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
